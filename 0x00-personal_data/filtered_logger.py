740851340833c33fd3e07a9712ae6b146a4d649e
ZafCode14 <zafarwp14@gmail.com>
2024-05-15 15:48:57 +0300

    pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,10 +10,13 @@ import mysql.connector
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     """Function that returns the log message"""
     for f in fields:
-        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+        message = re.sub(
+                f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
     return message
 
 
@@ -74,8 +77,10 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = filter_datum(
+                self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
 
 if __name__ == '__main__':
     main()
