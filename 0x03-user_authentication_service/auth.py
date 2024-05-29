531c34a1549d45e6abe435dbc4140fcf42d87a89
ZafCode14 <zafarwp14@gmail.com>
2024-05-29 16:46:33 +0300

    init

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,7 +23,7 @@ class Auth:
     """
 
     def __init__(self):
-        """intialization"""
+        """Method that inits the class"""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
